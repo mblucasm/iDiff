14aae1b9e0383be1744d5b06408636da1345482e
Lucas Martin <mblucasm@gmail.com>
2025-02-16 13:11:43 +0100

    Refactoring and added support to html files

--- src/main.c
+++ src/main.c
@@ -22,41 +22,60 @@
 #   define DELIM "/"
 #endif
 
-#define NFILES (2)
-#define DIV_PREF ("<div")
-#define DIV_PREFL (sizeof(DIV_PREF) / sizeof(DIV_PREF[0]) - 1)
+#define NFILES    (2)
+#define BUFCAP    (1024)
+#define PREF_DIV  ("<div")
+#define PREF_HTML ("<html>")
 
 #define FLAG_GETLIST1 ("--get-list-1")
 #define FLAG_GETLIST2 ("--get-list-2")
 #define FLAG_DETAHELP ("--detailed-help")
 
 typedef struct {
-    bool get_lists[2];
+    FILE *stream;
+    bool print;
+} ListOut;
+
+typedef struct {
+    ListOut lo[NFILES];
     const char *program;
-    const char *paths[2];
+    const char *paths[NFILES];
 } Args;
 
 Args args_parse(int *argc, char ***argv);
 char *arg_shift(int *argc, char ***argv);
 
 typedef struct {
+    char *buf;
+    size_t len;
+    size_t cap;
+} Buf;
+
+Buf buf_from_file_raw(const char *file_contents, size_t file_len, ListOut lo);
+Buf buf_from_file_html(const char *file_contents, size_t file_len, ListOut lo);
+Buf buf_from_file_div(const char *file_contents, size_t file_len, ListOut lo);
+bool bufput(Buf *buf, Slice slice);
+char *bufapp(Buf *buf, Slice slice);
+void buffree(Buf *buf);
+
+typedef struct {
     char *key;
     int value;
 } Dict;
 
-Dict *dict_from_list(char **list);
+Dict *dict_from_buf(Buf buf);
 
 void usage(void);
 void detahelp(void);
 void elog(Error error);
 void quit(int code);
 
+size_t compar_to_file_raw(Dict *dict, const char *file_contents, size_t file_len);
+size_t compar_to_file_html(Dict *dict, const char *file_contents, size_t file_len);
+size_t compar_to_file_div(Dict *dict, const char *file_contents, size_t file_len);
 char *file_read(const char *filePath, size_t *readedLen, Error *error);
-char **div_parse_into_list(char *div, size_t len);
-char **list_parse_into_list(char *contents, size_t len);
-void list_free(char **list);
-void list_print(char **list);
-bool starts_with(char *buffer, size_t len, char *prefix, size_t plen);
+bool starts_with(const char *buffer, size_t len, char *prefix, size_t plen);
+size_t maxst(size_t a, size_t b);
 
 // iDiff - Instagram User and List Difference Finder
 // 
@@ -69,46 +88,42 @@ bool starts_with(char *buffer, size_t len, char *prefix, size_t plen);
 // - Extract usernames from Instagram HTML snippets.
 // - Display missing users/items in a clean format.
 // 
-// Uses the stb_ds.h library for dynamic arrays and hash tables.
+// Uses the stb_ds.h library for hash tables.
 
 int main(int argc, char **argv) {
     
     Args args = args_parse(&argc, &argv);
     if(!args.paths[0] || !args.paths[1]) {usage(); quit(1);}
     
-    char *file_contents[NFILES];
-    size_t file_lens[NFILES];
-    for(size_t i = 0; i < NFILES; ++i) {
-        Error error;
-        file_contents[i] = file_read(args.paths[i], file_lens + i, &error);
-        if(!file_contents[i]) elog(error);    
-    }
+    Error error;
+    size_t file_len;
+    char *file_contents = file_read(args.paths[0], &file_len, &error);
+    if(!file_contents) elog(error);
     
-    char **lists[NFILES];
-    for(size_t i = 0; i < NFILES; ++i) {
-        char *buf = file_contents[i];
-        size_t len = file_lens[i];
-        lists[i] = starts_with(buf, len, DIV_PREF, DIV_PREFL) ? div_parse_into_list(buf, len) : list_parse_into_list(buf, len);
-    }
+    Buf buf;
+    if(starts_with(file_contents, file_len, PREF_HTML, strlen(PREF_HTML))) buf = buf_from_file_html(file_contents, file_len, args.lo[0]);
+    else if(starts_with(file_contents, file_len, PREF_DIV, strlen(PREF_DIV))) buf = buf_from_file_div(file_contents, file_len, args.lo[0]);
+    else buf = buf_from_file_raw(file_contents, file_len, args.lo[0]);
+    free(file_contents);
 
-    for(size_t i = 0; i < NFILES; ++i) {
-        size_t len = printf("==== LIST %zu (%zu INSTANCES) ====\n", i + 1, arrlenu(lists[i]));
-        if(args.get_lists[i]) list_print(lists[i]);
-        for(size_t j = 0; j < len - 1; ++j) printf("=");
-        printf("\n\n");
-        free(file_contents[i]);
-    }
-
-    Dict *dict = dict_from_list(lists[0]);
+    Dict *dict = dict_from_buf(buf);
+    file_contents = file_read(args.paths[1], &file_len, &error);
+    if(!file_contents) elog(error);
+    
+    size_t count;
     printf("===================NOT MATCHING LIST===================\n");
-    size_t count = 0;
-    for(size_t i = 0; i < arrlenu(lists[1]); ++i) if(shgeti(dict, lists[1][i]) == -1) printf("%zu >> %s\n", ++count, lists[1][i]);
+    if(starts_with(file_contents, file_len, PREF_HTML, strlen(PREF_HTML))) count = compar_to_file_html(dict, file_contents, file_len);
+    else if(starts_with(file_contents, file_len, PREF_DIV, strlen(PREF_DIV))) count = compar_to_file_div(dict, file_contents, file_len);
+    else count = compar_to_file_raw(dict, file_contents, file_len);
     printf("=================NOT MATCHING LIST END=================\n");
-    printf("-> Number of users that didn't match = %zu\n", count);
+    printf("Total mismatches = %zu\n", count);
 
+    free(file_contents);
     shfree(dict);
-    for(size_t i = 0; i < NFILES; ++i) list_free(lists[i]);
+    buffree(&buf);
+    for(size_t i = 0; i < NFILES; ++i) if(args.lo[i].stream) fclose(args.lo[i].stream);
     quit(0);
+    return 0;
 }
 
 void quit(int code) {
@@ -142,57 +157,10 @@ void usage(void) {
     printf("  %s       get the list 2 of instances\n", FLAG_GETLIST2);
 }
 
-char **div_parse_into_list(char *div, size_t len) {
-    char **list = NULL;
-    Slice slice = snewl(div, len);
-    while(slice.len > 0) {
-        (void)sslices(&slice, snew("alt=\""), true);
-        Slice name = sslice(&slice, '\'', true);
-        if(name.len > 0) {
-            char *seg = malloc(sizeof(char) * (name.len + 1));
-            memcpy(seg, name.ptr, sizeof(char) * name.len);
-            seg[name.len] = '\0';
-            arrput(list, seg);
-        }
-    } return list;
-}
-
-char **list_parse_into_list(char *contents, size_t len) {
-    char **list = NULL;
-    Slice s = snewl(contents, len);
-    while(s.len > 0) {
-        Slice name = sslice(&s, '\n', true);
-        Slice chop = sslices(&name, snew(">>"), true);
-        if(!name.len) name = chop;
-        name = strim(name);
-        if(name.len > 0) {
-            char *seg = malloc(sizeof(char) * (name.len + 1));
-            memcpy(seg, name.ptr, sizeof(char) * name.len);
-            seg[name.len] = '\0';
-            arrput(list, seg);
-        }
-    } return list;
-}
-
-Dict *dict_from_list(char **list) {
-    Dict *dict = NULL;
-    for(size_t i = 0; i < arrlenu(list); ++i) shput(dict, list[i], 1);
-    return dict;
-}
-
-bool starts_with(char *buffer, size_t len, char *prefix, size_t plen) {
+bool starts_with(const char *buffer, size_t len, char *prefix, size_t plen) {
     if(len < plen) return false;
     for(size_t i = 0; i < plen; ++i) if(prefix[i] != buffer[i]) return false;
     return true;
-}
-
-void list_free(char **list) {
-    for(size_t i = 0; i < arrlenu(list); ++i) free(list[i]);
-    arrfree(list);
-}
-
-void list_print(char **list) {
-    for(size_t i = 0; i < arrlenu(list); ++i) printf("%zu >> %s\n", i + 1, list[i]);
 }
 
 char *arg_shift(int *argc, char ***argv) {
@@ -235,17 +203,182 @@ Args args_parse(int *argc, char ***argv) {
     Args args = {0};
     args.program = arg_shift(argc, argv);
     while(*argc > 0) {
-        char *arg = arg_shift(argc, argv);
-        if  (strcmp(arg, FLAG_DETAHELP) == 0) {usage(); detahelp(); quit(0);}
-        else if(strcmp(arg, FLAG_GETLIST1) == 0) args.get_lists[0] = true;
-        else if(strcmp(arg, FLAG_GETLIST2) == 0) args.get_lists[1] = true;
-        else if(!args.paths[0]) args.paths[0] = arg;
-        else args.paths[1] = arg;
-    } return args;
+        Slice arg = snew(arg_shift(argc, argv));
+        if(strcmp(arg.ptr, FLAG_DETAHELP) == 0) {usage(); detahelp(); quit(0);}
+        else if(starts_with(arg.ptr, arg.len, FLAG_GETLIST1, strlen(FLAG_GETLIST1))) {
+            args.lo[0].print = true;
+            (void)sslice(&arg, '=', true);
+            if(arg.len > 0) {
+                args.lo[0].stream = fopen(arg.ptr, "wb");
+                if(args.lo[0].stream == NULL) {fprintf(stderr, "[ERROR]: Unable to open %s : %s\n", arg.ptr, strerror(errno)); goto defer1;}
+            }
+        } else if(starts_with(arg.ptr, arg.len, FLAG_GETLIST2, strlen(FLAG_GETLIST2))) {
+            args.lo[1].print = true;
+            (void)sslice(&arg, '=', true);
+            if(arg.len > 0) {
+                args.lo[1].stream = fopen(arg.ptr, "wb");
+                if(args.lo[1].stream == NULL) {fprintf(stderr, "[ERROR]: Unable to open %s : %s\n", arg.ptr, strerror(errno)); goto defer2;}
+            }
+        } else if(!args.paths[0]) args.paths[0] = arg.ptr;
+        else args.paths[1] = arg.ptr;
+    } 
+    return args;
+    defer2:
+        if(args.lo[0].stream) fclose(args.lo[0].stream);
+    defer1:
+        quit(1);
+        return (Args){0};
 }
 
 void elog(Error error) {
-    fprintf(stderr, "[INSTA_ERROR]: Couldn't read from file '%s' : %s\n", error.info[0], error.info[1]);
-    fprintf(stderr, "[INSTA_ERROR]: Got error '%s'\n", etochar(error.type));
+    fprintf(stderr, "[ERROR]: Couldn't read from file '%s' : %s\n", error.info[0], error.info[1]);
+    fprintf(stderr, "[ERROR]: Got error '%s'\n", etochar(error.type));
     quit(1);
+}
+
+size_t maxst(size_t a, size_t b) {
+    return a < b ? b : a;
+}
+
+char *bufapp(Buf *buf, Slice slice) {
+    if(buf->cap < buf->len + slice.len + 1) {
+        size_t newcap = buf->cap + maxst(BUFCAP, slice.len + 1);
+        char *dst = realloc(buf->buf, sizeof(char) * newcap);
+        if(dst == NULL) return NULL;
+        buf->cap = newcap;
+        buf->buf = dst;
+    } 
+    char *dst = buf->buf + (sizeof(char) * buf->len);
+    memcpy(dst, slice.ptr, sizeof(char) * slice.len);
+    buf->len += slice.len;
+    buf->buf[buf->len++] = '\0';
+    return dst;
+}
+
+void buffree(Buf *buf) {
+    free(buf->buf);
+    buf->buf = NULL;
+    buf->cap = 0;
+    buf->len = 0;
+}
+
+bool bufput(Buf *buf, Slice slice) {
+    if(buf->cap < slice.len + 1) {
+        size_t newcap = buf->cap + maxst(BUFCAP, slice.len + 1);
+        char *dst = realloc(buf->buf, sizeof(char) * newcap);
+        if(dst == NULL) return false;
+        buf->cap = newcap;
+        buf->buf = dst;
+    } 
+    memcpy(buf->buf, slice.ptr, sizeof(char) * slice.len);
+    buf->len = slice.len;
+    buf->buf[buf->len] = '\0';
+    return true;
+}
+
+Dict *dict_from_buf(Buf buf) {
+    Dict *dict = NULL;
+    char *ptr = buf.buf;
+    while(ptr < buf.buf + buf.len) {
+        shput(dict, ptr, 1);
+        ptr += strlen(ptr) + 1;
+    } return dict;
+}
+
+Buf buf_from_file_raw(const char *file_contents, size_t file_len, ListOut lo) {
+    Buf buf = {0};
+    size_t count = 0;
+    Slice slice = snewl(file_contents, file_len);
+    while(slice.len > 0) {
+        Slice name = sslice(&slice, '\n', true);
+        Slice chop = sslices(&name, snew(">>"), true);
+        if(!name.len) name = chop;
+        name = strim(name);
+        if(name.len > 0) {
+            char *dst = bufapp(&buf, name);
+            if(lo.print) fprintf(lo.stream ? lo.stream : stdout, "%zu >> %s\n", ++count, dst);
+            if(!dst) {fprintf(stderr, "[ERROR]: Ran out of memory, list too long\n"); quit(1);}
+        }
+    } return buf;
+}
+
+Buf buf_from_file_html(const char *file_contents, size_t file_len, ListOut lo) {
+    Buf buf = {0};
+    size_t count = 0;
+    Slice dcs = snew(".com/");
+    Slice slice = snewl(file_contents, file_len);
+    while(slice.len > 0) {
+        (void)sslices(&slice, dcs, true);
+        Slice name = strim(sslice(&slice, '"', true));
+        if(name.len > 0) {
+            char *dst = bufapp(&buf, name);
+            if(lo.print) fprintf(lo.stream ? lo.stream : stdout, "%zu >> %s\n", ++count, dst);
+            if(!dst) {fprintf(stderr, "[ERROR]: Ran out of memory, list too long\n"); quit(1);}
+        }
+    } return buf;
+}
+
+Buf buf_from_file_div(const char *file_contents, size_t file_len, ListOut lo) {
+    Buf buf = {0};
+    size_t count = 0;
+    Slice aeq = snew("alt=\"");
+    Slice slice = snewl(file_contents, file_len);
+    while(slice.len > 0) {
+        (void)sslices(&slice, aeq, true);
+        Slice name = strim(sslice(&slice, '\'', true));
+        if(name.len > 0) {
+            char *dst = bufapp(&buf, name);
+            if(lo.print) fprintf(lo.stream ? lo.stream : stdout, "%zu >> %s\n", ++count, dst);
+            if(!dst) {fprintf(stderr, "[ERROR]: Ran out of memory, list too long\n"); quit(1);}
+        }
+    } return buf;
+}
+
+size_t compar_to_file_raw(Dict *dict, const char *file_contents, size_t file_len) {
+    Buf buf = {0};
+    size_t count = 0;
+    Slice slice = snewl(file_contents, file_len);
+    while(slice.len > 0) {
+        Slice name = sslice(&slice, '\n', true);
+        Slice chop = sslices(&name, snew(">>"), true);
+        if(!name.len) name = chop;
+        name = strim(name);
+        if(name.len > 0) {
+            bufput(&buf, name);
+            if(shgeti(dict, buf.buf) == -1) printf("%zu >> %.*s\n", ++count, (int)name.len, name.ptr);
+        }
+    } buffree(&buf);
+    return count;
+}
+
+size_t compar_to_file_html(Dict *dict, const char *file_contents, size_t file_len) {
+    Buf buf = {0};
+    size_t count = 0;
+    Slice dcs = snew(".com/");
+    Slice slice = snewl(file_contents, file_len);
+    while(slice.len > 0) {
+        (void)sslices(&slice, dcs, true);
+        Slice name = strim(sslice(&slice, '"', true));
+        if(name.len > 0) {
+            bufput(&buf, name);
+            if(shgeti(dict, buf.buf) == -1) printf("%zu >> %.*s\n", ++count, (int)name.len, name.ptr);
+        }
+    } buffree(&buf);
+    return count;
+}
+
+size_t compar_to_file_div(Dict *dict, const char *file_contents, size_t file_len) {
+    Buf buf = {0};
+    size_t count = 0;
+    Slice aeq = snew("alt=\"");
+    Slice slice = snewl(file_contents, file_len);
+    while(slice.len > 0) {
+        (void)sslices(&slice, aeq, true);
+        Slice name = strim(sslice(&slice, '\'', true));
+        if(name.len > 0) {
+            bufput(&buf, name);
+            if(shgeti(dict, buf.buf) == -1) printf("%zu >> %.*s\n", ++count, (int)name.len, name.ptr);
+        }
+    } buffree(&buf);
+    return count;
 }
