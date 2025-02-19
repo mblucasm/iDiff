8dcb81e4ce667058c01ce0700c942fa12913993d
Lucas Martin <mblucasm@gmail.com>
2025-02-19 21:49:48 +0100

    bufcatS && global args

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <assert.h>
+#include <stdarg.h>
 
 #include "include/slice.h"
 #include "include/error.h"
@@ -35,17 +36,15 @@
 #define DEFAULT_STREAM2 ("iDiff-list2.txt")
 
 #define FLAG_HELP     ("--help")
-#define FLAG_GETLIST  ("--get-list-")
+#define FLAG_DUMPLIST ("--dump-list-")
 #define FLAG_DETAHELP ("--detailed-help")
 #define FLAG_IFOLDER  ("--instagram-folder=")
 
-#define FLAG_IFOLDERLEN (sizeof(FLAG_IFOLDER) / sizeof(FLAG_IFOLDER[0]) - 1)
-#define FLAG_GETLISTLEN (sizeof(FLAG_GETLIST) / sizeof(FLAG_GETLIST[0]) - 1)
+#define FLAG_IFOLDERLEN  (sizeof(FLAG_IFOLDER) / sizeof(FLAG_IFOLDER[0]) - 1)
+#define FLAG_DUMPLISTLEN (sizeof(FLAG_DUMPLIST) / sizeof(FLAG_DUMPLIST[0]) - 1)
 
 #define IPATH ("/connections/followers_and_following/")
 #define IPATHLEN (sizeof(IPATH) / sizeof(IPATH[0]) - 1)
-
-const char * const DEFAULT_STREAMS[2] = {DEFAULT_STREAM1, DEFAULT_STREAM2};
 
 typedef struct {
     bool ifolder;
@@ -54,7 +53,7 @@ typedef struct {
     FILE *list_streams[NFILES];
 } Args;
 
-Args args_parse(int *argc, char ***argv);
+void args_parse(int *argc, char ***argv);
 char *arg_shift(int *argc, char ***argv);
 
 typedef struct {
@@ -63,14 +62,18 @@ typedef struct {
     size_t cap;
 } Buf;
 
-Buf buf_from_file_raw(const char *file_contents, size_t file_len, FILE *stream);
-Buf buf_from_file_div(const char *file_contents, size_t file_len, FILE *stream);
-Buf buf_from_file_html(const char *file_contents, size_t file_len, FILE *stream);
-bool bufput(Buf *buf, Slice slice);
-char *bufapp(Buf *buf, Slice slice);
-char *bufcat(Buf *buf, Slice slice);
-void buffree(Buf *buf);
-void replacec(char *buffer, size_t len, const char old, const char new);
+#define bufcatS(bufp, ...) _bufcatS((bufp), __VA_ARGS__, (Slice){0})
+
+Buf buf_from_file_raw  (const char *file_contents, size_t file_len, FILE *stream);
+Buf buf_from_file_div  (const char *file_contents, size_t file_len, FILE *stream);
+Buf buf_from_file_html (const char *file_contents, size_t file_len, FILE *stream);
+
+bool bufputs  (Buf *buf, Slice slice);
+bool bufcats  (Buf *buf, Slice slice);
+bool _bufcatS (Buf *buf, ...);
+char *bufapps (Buf *buf, Slice slice);
+void buffree  (Buf *buf);
+
 void args_get_paths_from_folder(const char *args_paths[NFILES], Slice folder);
 
 typedef struct {
@@ -100,6 +103,7 @@ void usage(void);
 void detahelp(void);
 void elog(Error error);
 void quit(int code);
+void replacec(char *buffer, size_t len, const char old, const char new);
 
 // iDiff - Instagram User and List Difference Finder
 // 
@@ -114,35 +118,38 @@ void quit(int code);
 // 
 // Uses the stb_ds.h library for hash tables.
 
+const char * const DEFAULT_STREAMS[2] = {DEFAULT_STREAM1, DEFAULT_STREAM2};
+Args gargs = {0};
+
 int main(int argc, char **argv) {
 
-    Args args = args_parse(&argc, &argv);
-    if(!args.paths[0] || !args.paths[1]) {usage(); quit(1);}
+    args_parse(&argc, &argv);
+    if(!gargs.paths[0] || !gargs.paths[1]) {usage(); quit(1);}
     
     Error error;
     size_t file_len;
-    char *file_contents = file_read(args.paths[0], &file_len, &error);
+    char *file_contents = file_read(gargs.paths[0], &file_len, &error);
     if(!file_contents) elog(error);
     
     Buf buf;
     Prefix pref = get_prefix(file_contents, file_len);
     switch(pref) {
-        case PREFIX_NONE: buf = buf_from_file_raw(file_contents, file_len, args.list_streams[0]); break;
-        case PREFIX_HTML: buf = buf_from_file_html(file_contents, file_len, args.list_streams[0]); break;
-        case PREFIX_DIV:  buf = buf_from_file_div(file_contents, file_len, args.list_streams[0]); break;
+        case PREFIX_NONE: buf = buf_from_file_raw(file_contents, file_len, gargs.list_streams[0]); break;
+        case PREFIX_HTML: buf = buf_from_file_html(file_contents, file_len, gargs.list_streams[0]); break;
+        case PREFIX_DIV:  buf = buf_from_file_div(file_contents, file_len, gargs.list_streams[0]); break;
     } free(file_contents);
 
     Dict *dict = dict_from_buf(buf);
-    file_contents = file_read(args.paths[1], &file_len, &error);
+    file_contents = file_read(gargs.paths[1], &file_len, &error);
     if(!file_contents) elog(error);
     
     printf("===================NOT MATCHING LIST===================\n");
     size_t count = 0;
     pref = get_prefix(file_contents, file_len);
     switch(pref) {
-        case PREFIX_NONE: count = compar_to_file_raw(dict, file_contents, file_len, args.list_streams[1]); break;
-        case PREFIX_HTML: count = compar_to_file_html(dict, file_contents, file_len, args.list_streams[1]); break;
-        case PREFIX_DIV:  count = compar_to_file_div(dict, file_contents, file_len, args.list_streams[1]); break;
+        case PREFIX_NONE: count = compar_to_file_raw(dict, file_contents, file_len, gargs.list_streams[1]); break;
+        case PREFIX_HTML: count = compar_to_file_html(dict, file_contents, file_len, gargs.list_streams[1]); break;
+        case PREFIX_DIV:  count = compar_to_file_div(dict, file_contents, file_len, gargs.list_streams[1]); break;
     }
     printf("=================NOT MATCHING LIST END=================\n");
     printf("Total mismatches = %zu\n", count);
@@ -150,8 +157,8 @@ int main(int argc, char **argv) {
     free(file_contents);
     shfree(dict);
     buffree(&buf);
-    for(size_t i = 0; i < NFILES; ++i) if(args.list_streams[i]) fclose(args.list_streams[i]);
-    if(args.ifolder) for(size_t i = 0; i < NFILES; ++i) free((char*)args.paths[i]);
+    for(size_t i = 0; i < NFILES; ++i) if(gargs.list_streams[i]) fclose(gargs.list_streams[i]);
+    if(gargs.ifolder) for(size_t i = 0; i < NFILES; ++i) free((char*)gargs.paths[i]);
     quit(0);
     return 0;
 }
@@ -187,8 +194,8 @@ void usage(void) {
     printf("Options:\n");
     printf("  %s             print this help message and quit\n", FLAG_HELP);
     printf("  %s    print the detailed help message and quit\n", FLAG_DETAHELP);
-    printf("  %s1       get the list 1 of instances, if followed by =<path> to a custom path (default %s)\n", FLAG_GETLIST, DEFAULT_STREAMS[0]);
-    printf("  %s2       get the list 2 of instances, if followed by =<path> to a custom path (default %s)\n", FLAG_GETLIST, DEFAULT_STREAMS[1]);
+    printf("  %s1      dump the list 1 of instances to %s, if followed by =<file> (%s1=<file>) to a custom file \n", FLAG_DUMPLIST, DEFAULT_STREAMS[0], FLAG_DUMPLIST);
+    printf("  %s2      dump the list 2 of instances to %s, if followed by =<file> (%s2=<file>) to a custom file \n", FLAG_DUMPLIST, DEFAULT_STREAMS[1], FLAG_DUMPLIST);
 }
 
 bool starts_with(const char *buffer, size_t len, char *prefix, size_t plen) {
@@ -235,15 +242,15 @@ char *file_read(const char *filePath, size_t *readedLen, Error *error) {
 
 FILE *get_list_stream(Slice arg, const char *default_stream) {
     const char *path = default_stream;
-    if(FLAG_GETLISTLEN + 2 < arg.len) {
-        if(arg.ptr[FLAG_GETLISTLEN + 1] == '=') path = arg.ptr + FLAG_GETLISTLEN + 2;
+    if(FLAG_DUMPLISTLEN + 2 < arg.len) {
+        if(arg.ptr[FLAG_DUMPLISTLEN + 1] == '=') path = arg.ptr + FLAG_DUMPLISTLEN + 2;
         else {
-            fprintf(stderr, "[ERROR]: %sX can only be followed by '=', got '%c' instead\n", FLAG_GETLIST, arg.ptr[FLAG_GETLISTLEN + 1]);
+            fprintf(stderr, "[ERROR]: %sX can only be followed by '=', got '%c' instead\n", FLAG_DUMPLIST, arg.ptr[FLAG_DUMPLISTLEN + 1]);
             quit(1);
         }
-    } else if(FLAG_GETLISTLEN + 2 == arg.len) {
-        if(arg.ptr[FLAG_GETLISTLEN + 1] == '=') fprintf(stderr, "[ERROR]: Expected a path after the '=' in %.*s\n", (int)arg.len, arg.ptr);
-        else fprintf(stderr, "[ERROR]: %sX can only be followed by '=', got '%c' instead\n", FLAG_GETLIST, arg.ptr[FLAG_GETLISTLEN + 1]);
+    } else if(FLAG_DUMPLISTLEN + 2 == arg.len) {
+        if(arg.ptr[FLAG_DUMPLISTLEN + 1] == '=') fprintf(stderr, "[ERROR]: Expected a path after the '=' in %.*s\n", (int)arg.len, arg.ptr);
+        else fprintf(stderr, "[ERROR]: %sX can only be followed by '=', got '%c' instead\n", FLAG_DUMPLIST, arg.ptr[FLAG_DUMPLISTLEN + 1]);
         quit(1);
     }
     FILE *f = fopen(path, "wb");
@@ -258,55 +265,49 @@ void replacec(char *buffer, size_t len, const char old, const char new) {
 }
 
 void args_get_paths_from_folder(const char *args_paths[NFILES], Slice folder) {
-    if(folder.ptr[folder.len - 1] == DELIMC || folder.ptr[folder.len - 1] == ANTIDELIMC) --folder.len;
+    while(folder.len > 1 && (folder.ptr[folder.len - 1] == DELIMC || folder.ptr[folder.len - 1] == ANTIDELIMC)) --folder.len;
     Buf buf1 = {0};
-    bufput(&buf1, folder);
-    bufcat(&buf1, snewl(IPATH, IPATHLEN));
-    bufcat(&buf1, snew("followers_1.html"));
     Buf buf2 = {0};
-    bufput(&buf2, folder);
-    bufcat(&buf2, snewl(IPATH, IPATHLEN));
-    bufcat(&buf2, snew("following.html"));
+    bufcatS(&buf1, folder, snewl(IPATH, IPATHLEN), snew("followers_1.html"));
+    bufcatS(&buf2, folder, snewl(IPATH, IPATHLEN), snew("following.html"));
     replacec(buf1.buf, buf1.len, ANTIDELIMC, DELIMC);
     replacec(buf2.buf, buf2.len, ANTIDELIMC, DELIMC);
     args_paths[0] = buf1.buf;
     args_paths[1] = buf2.buf;
 }
 
-Args args_parse(int *argc, char ***argv) {
-    Args args = {0};
-    args.program = arg_shift(argc, argv);
+void args_parse(int *argc, char ***argv) {
+    gargs = (Args){0};
+    gargs.program = arg_shift(argc, argv);
     while(*argc > 0) {
         Slice arg = snew(arg_shift(argc, argv));
         if(strcmp(arg.ptr, FLAG_HELP) == 0) {usage(); quit(0);}
         else if(strcmp(arg.ptr, FLAG_DETAHELP) == 0) {usage(); detahelp(); quit(0);}
         else if(starts_with(arg.ptr, arg.len, FLAG_IFOLDER, FLAG_IFOLDERLEN)) {
-            args.ifolder = true;
+            gargs.ifolder = true;
             (void)sslice(&arg, '=', true);
-            if(arg.len > 0) args_get_paths_from_folder(args.paths, arg);
+            if(arg.len > 0) args_get_paths_from_folder(gargs.paths, arg);
             else {
                 fprintf(stderr, "[ERROR]: '%s' flag NEEDS to be followed by a folder path\n", FLAG_IFOLDER);
                 quit(1);
             }
-        } else if(starts_with(arg.ptr, arg.len, FLAG_GETLIST, FLAG_GETLISTLEN)) {
-            if(!(arg.len == FLAG_GETLISTLEN || (arg.ptr[FLAG_GETLISTLEN] != '1' && arg.ptr[FLAG_GETLISTLEN] != '2'))) {
-                int target = arg.ptr[FLAG_GETLISTLEN] - '1';
-                if(args.list_streams[target]) fclose(args.list_streams[target]);
-                args.list_streams[target] = get_list_stream(arg, DEFAULT_STREAMS[target]);
-                if(args.list_streams[target] == NULL) goto defer;
+        } else if(starts_with(arg.ptr, arg.len, FLAG_DUMPLIST, FLAG_DUMPLISTLEN)) {
+            if(!(arg.len == FLAG_DUMPLISTLEN || (arg.ptr[FLAG_DUMPLISTLEN] != '1' && arg.ptr[FLAG_DUMPLISTLEN] != '2'))) {
+                int target = arg.ptr[FLAG_DUMPLISTLEN] - '1';
+                if(gargs.list_streams[target]) fclose(gargs.list_streams[target]);
+                gargs.list_streams[target] = get_list_stream(arg, DEFAULT_STREAMS[target]);
+                if(gargs.list_streams[target] == NULL) goto defer;
             } else {
-                fprintf(stderr, "[ERROR]: '%s' flag NEEDS to be followed by either a 1 or a 2: %s1 or %s2\n", FLAG_GETLIST, FLAG_GETLIST, FLAG_GETLIST);
+                fprintf(stderr, "[ERROR]: '%s' flag NEEDS to be followed by either a 1 or a 2: %s1 or %s2\n", FLAG_DUMPLIST, FLAG_DUMPLIST, FLAG_DUMPLIST);
                 fprintf(stderr, "[ERROR]: Aditionally it CAN be followed by =path, where path is the desired output path for that specific list\n");
                 quit(1);
             }
-        } else if(!args.paths[0]) args.paths[0] = arg.ptr;
-        else args.paths[1] = arg.ptr;
+        } else if(!gargs.paths[0]) gargs.paths[0] = arg.ptr;
+        else gargs.paths[1] = arg.ptr;
     }
-    return args;
     defer:
-        for(size_t i = 0; i < NFILES; ++i) if(args.list_streams[i]) fclose(args.list_streams[i]);
+        for(size_t i = 0; i < NFILES; ++i) if(gargs.list_streams[i]) fclose(gargs.list_streams[i]);
         quit(1);
-        return (Args){0};
 }
 
 void elog(Error error) {
@@ -319,7 +320,7 @@ size_t maxst(size_t a, size_t b) {
     return a < b ? b : a;
 }
 
-bool bufput(Buf *buf, Slice slice) {
+bool bufputs(Buf *buf, Slice slice) {
     if(buf->cap < slice.len + 1) {
         size_t newcap = buf->cap + maxst(BUFCAP, slice.len + 1);
         char *dst = realloc(buf->buf, sizeof(char) * newcap);
@@ -333,7 +334,7 @@ bool bufput(Buf *buf, Slice slice) {
     return true;
 }
 
-char *bufapp(Buf *buf, Slice slice) {
+char *bufapps(Buf *buf, Slice slice) {
     if(buf->cap < buf->len + 1 + slice.len + 1) {
         size_t newcap = buf->cap + maxst(BUFCAP, 1 + slice.len + 1);
         char *dst = realloc(buf->buf, sizeof(char) * newcap);
@@ -348,19 +349,18 @@ char *bufapp(Buf *buf, Slice slice) {
     return dst;
 }
 
-char *bufcat(Buf *buf, Slice slice) {
+bool bufcats(Buf *buf, Slice slice) {
     if(buf->cap < buf->len + slice.len + 1) {
         size_t newcap = buf->cap + maxst(BUFCAP, slice.len + 1);
         char *dst = realloc(buf->buf, sizeof(char) * newcap);
-        if(dst == NULL) return NULL;
+        if(dst == NULL) return false;
         buf->cap = newcap;
         buf->buf = dst;
     } 
-    char *dst = buf->buf + (sizeof(char) * buf->len);
-    memcpy(dst, slice.ptr, sizeof(char) * slice.len);
+    memcpy(buf->buf + (sizeof(char) * buf->len), slice.ptr, sizeof(char) * slice.len);
     buf->len += slice.len;
     buf->buf[buf->len] = '\0';
-    return dst;
+    return true;
 }
 
 void buffree(Buf *buf) {
@@ -389,7 +389,7 @@ Buf buf_from_file_raw(const char *file_contents, size_t file_len, FILE *stream) 
         if(!name.len) name = chop;
         name = strim(name);
         if(name.len > 0) {
-            char *dst = bufapp(&buf, name);
+            char *dst = bufapps(&buf, name);
             if(stream) fprintf(stream, "%zu >> %s\n", ++count, dst);
             if(!dst) {fprintf(stderr, "[ERROR]: Ran out of memory, list too long\n"); quit(1);}
         }
@@ -405,7 +405,7 @@ Buf buf_from_file_html(const char *file_contents, size_t file_len, FILE *stream)
         (void)sslices(&slice, dcs, true);
         Slice name = strim(sslice(&slice, '"', true));
         if(name.len > 0) {
-            char *dst = bufapp(&buf, name);
+            char *dst = bufapps(&buf, name);
             if(stream) fprintf(stream, "%zu >> %s\n", ++count, dst);
             if(!dst) {fprintf(stderr, "[ERROR]: Ran out of memory, list too long\n"); quit(1);}
         }
@@ -422,7 +422,7 @@ Buf buf_from_file_div(const char *file_contents, size_t file_len, FILE *stream) 
         (void)sslices(&slice, aeq, true);
         Slice name = strim(sslice(&slice, '\'', true));
         if(name.len > 0) {
-            char *dst = bufapp(&buf, name);
+            char *dst = bufapps(&buf, name);
             if(stream) fprintf(stream, "%zu >> %s\n", ++count, dst);
             if(!dst) {fprintf(stderr, "[ERROR]: Ran out of memory, list too long\n"); quit(1);}
         }
@@ -440,7 +440,7 @@ size_t compar_to_file_raw(Dict *dict, const char *file_contents, size_t file_len
         if(!name.len) name = chop;
         name = strim(name);
         if(name.len > 0) {
-            bufput(&buf, name);
+            bufputs(&buf, name);
             if(stream) fprintf(stream, "%zu >> %s\n", ++count2, buf.buf);
             if(shgeti(dict, buf.buf) == -1) printf("%zu >> %.*s\n", ++count, (int)name.len, name.ptr);
         }
@@ -458,7 +458,7 @@ size_t compar_to_file_html(Dict *dict, const char *file_contents, size_t file_le
         (void)sslices(&slice, dcs, true);
         Slice name = strim(sslice(&slice, '"', true));
         if(name.len > 0) {
-            bufput(&buf, name);
+            bufputs(&buf, name);
             if(stream) fprintf(stream, "%zu >> %s\n", ++count2, buf.buf);
             if(shgeti(dict, buf.buf) == -1) printf("%zu >> %.*s\n", ++count, (int)name.len, name.ptr);
         }
@@ -477,7 +477,7 @@ size_t compar_to_file_div(Dict *dict, const char *file_contents, size_t file_len
         (void)sslices(&slice, aeq, true);
         Slice name = strim(sslice(&slice, '\'', true));
         if(name.len > 0) {
-            bufput(&buf, name);
+            bufputs(&buf, name);
             if(stream) fprintf(stream, "%zu >> %s\n", ++count2, buf.buf);
             if(shgeti(dict, buf.buf) == -1) printf("%zu >> %.*s\n", ++count, (int)name.len, name.ptr);
         }
@@ -499,4 +499,15 @@ void div_ensure_english(Slice contents) {
         fprintf(stderr, "[NOTE]: It could also be that no followers/ing where present on that <div> file...\n");
         quit(1);
     }
+}
+
+bool _bufcatS(Buf *buf, ...) {
+    Slice s;
+    va_list va;
+    va_start(va, buf);
+    bool ret = true;
+    while((s = va_arg(va, Slice)).ptr != NULL) if(!bufcats(buf, s)) {ret = false; goto defer;}
+    defer:
+        va_end(va);
+        return ret;
 }
